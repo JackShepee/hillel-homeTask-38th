abc820017b5177eb01b08b266a5b10943cef683c
VSalamakha <salamakhaviktor@gmail.com>
2023-09-30 13:23:29 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+class Product {
+    constructor(id, name, price, isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.isAvailable = isAvailable;
+    }
+}
+
+class DiscountedProduct extends Product {
+    constructor(id, name, price, isAvailable, discountPercentage) {
+        super(id, name, price, isAvailable);
+        this.discountPercentage = discountPercentage;
+    }
+
+    getDiscountedPrice() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (this.isAvailable) {
+                    const discountedPrice = this.price - (this.price * this.discountPercentage / 100);
+                    resolve(discountedPrice);
+                } else {
+                    reject("Товар не доступний");
+                }
+            }, 1000);
+        });
+    }
+}
+
+const product = new DiscountedProduct(1, "Футболка", 20.0, true, 10);
+product
+    .getDiscountedPrice()
+    .then((discountedPrice) => {
+        console.log(`Ціна зі знижкою: $${discountedPrice}`);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+
+const product2 = new DiscountedProduct(2, "Пальто", 100.5, true, 25);
+product2
+    .getDiscountedPrice()
+    .then((discountedPrice) => {
+        console.log(`Ціна зі знижкою: $${discountedPrice}`);
+    })
+    .catch((error) => {
+        console.error(error)
+    });
